05ddd440fa682b81f91e8566b9e70262469a86ef
chl.cken <ckdgus988@naver.com>
2023-04-11 10:40:23 +0900

    REFACTOR: 알고리즘 구현 최종

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,32 @@ package com.company;
 
 import java.util.*;
 
-/**
- * TODO 제출전: 맞춤법, 주석, 변수,메소드 명 체크
- * TODO 예외 처리(puzzleSize == 0, puzzleSize == 1, puzzle 입력값이 size 넘는 경우, 음수가 들어오는 경우, puzzleSize = 3이상이 들어오는 경우 A* 알고리즘 alert)
- * TODO 시간 계산 넣기
- * TODO 3입력시 랜덤 3 *3 퍼즐 생성하는걸로? 아님 입력?
- * solvalbleCheck 역전 카운트란 초기 상태와 목표 상태에서 이동 가능유무를 파악하는 조건 -> 적용 안될 때 return -1과 print 메시지, 초기화를 해주자.
- */
+/*
+*
+* 과제 예제
+3
+1 0 3
+4 2 5
+7 8 6
+* 되는경우
+3
+1 2 3
+4 0 5
+7 8 6
+* 안되는 경우
+3
+1 2 3
+4 0 5
+6 7 8
+*
+* */
 public class Main {
 
-    private int createPuzzleItem(Scanner sc) throws IllegalArgumentException {
+    private int createPuzzleItem(Scanner sc) {
         int puzzleSize = sc.nextInt();
 
-        if (puzzleSize == 0 || puzzleSize == 1) {
-            throw new IllegalArgumentException("0 또는 1을 입력할 수 없습니다.");
+        if (puzzleSize == 0) {
+            return -598;
         }
 
         int[][] puzzleArr = new int[puzzleSize][puzzleSize];
@@ -24,14 +36,14 @@ public class Main {
             for (int j = 0; j < puzzleSize; j++) {
                 int value = sc.nextInt();
                 if (value < 0 || value >= puzzleSize * puzzleSize) {
-                    throw new IllegalArgumentException("잘못된 퍼즐 사이즈를 입력하였습니다.");
+                    return -599;
                 }
                 puzzleArr[i][j] = value;
             }
         }
-
         System.out.println();
-        return isSolvablePuzzleCheck(puzzleSize, puzzleArr);
+
+        return solvePuzzleCheck(puzzleSize, puzzleArr);
     }
 
     private int[] transformNonLinearToLinear(int puzzleLength, int[][] puzzleArr) {
@@ -47,8 +59,7 @@ public class Main {
         return linearPuzzleArr;
     }
 
-    // 얼리 리턴 적용
-    private int isSolvablePuzzleCheck(int puzzleSize, int[][] puzzleArr) {
+    private int solvePuzzleCheck(int puzzleSize, int[][] puzzleArr) {
         int puzzleLength = puzzleSize * puzzleSize;
         int[] linearPuzzleArr = transformNonLinearToLinear(puzzleLength, puzzleArr);
         int inversionCount = calculateInversionCount(puzzleLength, linearPuzzleArr);
@@ -89,7 +100,6 @@ public class Main {
         return false;
     }
 
-    //TODO insert sort -> merge sort로 변경하기
     private int calculateInversionCount(int puzzleLength, int[] linearPuzzleArr) {
         int inversionCount = 0;
 
@@ -154,7 +164,6 @@ public class Main {
         int minimumValue = 987654321;
         int nextZeroDir = 0;
 
-        //상우하좌 탐색 하며, 탐색 상태의 0의 위치를 만드는 배열 newArr를 통해 복사전 배열 생성
         for (int dir = 0; dir < 4; dir++) {
             int newRow = zeroRow + dRow[dir];
             int newCol = zeroCol + dCol[dir];
@@ -217,6 +226,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Main main = new Main();
         int puzzle = main.createPuzzleItem(sc);
-        System.out.println("총 이동횟수: " + puzzle + "번");
+
+        if (puzzle == -1) {
+            System.out.println("해결할 수 없는 퍼즐입니다.");
+        } else if (puzzle == -598) {
+            System.out.println("퍼즐 사이즈가 0이 될 수 없습니다.");
+        } else if (puzzle == -599) {
+            System.out.println("입력 값을 확인해주세요.");
+        } else {
+            System.out.println("총 이동횟수: " + puzzle + "번");
+        }
     }
 }
